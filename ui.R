38c46744bfb9542558c374ac2d3392b127bdc34e
Tim Bowler <timbowler@verizon.net>
2017-05-16 16:47:03 -0400

    Add files via upload

---
+++ ui.R
@@ -0,0 +1,14 @@
+library(shiny)
+
+pageWithSidebar(
+      headerPanel('Iris k-means clustering'),
+      sidebarPanel(
+            selectInput('xcol', 'X Variable', names(iris[1:4])),
+            selectInput('ycol', 'Y Variable', names(iris[1:4]), 
+                        selected = names(iris)[2]),
+            numericInput('clusters', 'Cluster count', 3, min = 1, max = 9)
+      ),
+      mainPanel(
+            plotOutput('plot1')
+      )
+)
